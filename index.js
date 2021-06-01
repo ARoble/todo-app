709aecdf7ff5c7c52ad364dfa573a96da3c7d060
Return Dev Bootcamp <45190868+ARoble@users.noreply.github.com>
2021-06-01 17:31:01 +0300

    database fix

--- index.js
+++ index.js
@@ -20,9 +20,9 @@ const db = mysql.createConnection({
 });
 
 db.connect((err) => {
-  if (err) {
-    throw err;
-  }
+  // if (err) {
+  //   throw err;
+  // }
   console.log("Connection has been established");
 });
 
