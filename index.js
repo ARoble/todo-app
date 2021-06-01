3c307447c385af1d221eda7419266530e3deb861
Return Dev Bootcamp <45190868+ARoble@users.noreply.github.com>
2021-06-01 17:27:45 +0300

    database fix

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ const db = mysql.createConnection({
   user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE,
+  socketPath: "/Applications/MAMP/tmp/mysql/mysql.sock",
 });
 
 db.connect((err) => {
